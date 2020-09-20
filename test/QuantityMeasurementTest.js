394f0353777b51952c58e3d4982adc0e15badfa1
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-21 03:08:09 +0530

    ADDED: Test case to compare Feet and Feet

--- test/QuantityMeasurementTest.js
+++ test/QuantityMeasurementTest.js
@@ -5,11 +5,23 @@ var quantityMeasurement = new QuantityMeasurement();
 var unit = new Unit();
 
 describe("Quantity Measurement", () => {
-    it("given0FeetAnd0Inch_WhenObjectSame_ShouldReturnTrue"), () => {
+    it("given0FeetAnd0Inch_WhenCheckedForType_ShouldReturnEqual"), () => {
         let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
         let quantity2 = new QuantityMeasurement(unit.Length.INCH, 0);
         let result1 = quantity1 instanceof QuantityMeasurement;
         let result2 = quantity2 instanceof QuantityMeasurement;
         assert.equal(result1, result2);
     } 
+    it("given0FeetAnd1Feet_WhenCompared_ShouldReturnFalse"), () => {
+        let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
+        let quantity2 = new QuantityMeasurement(unit.Length.Feet, 1);
+        let compareCheck = quantityMeasurement.compare(quantity1, quantity2);
+        assert.isNotTrue(compareCheck);
+    }
+    it("given0FeetAnd0Feet_WhenCompared_ShouldReturnTrue"), () => {
+        let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
+        let quantity2 = new QuantityMeasurement(unit.Length.Feet, 0);
+        let compareCheck = quantityMeasurement.compare(quantity1, quantity2);
+        assert.isTrue(compareCheck);
+    }  
 });
