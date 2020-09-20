3e5a3b0ef9ffa2b38086441c69d921c7259029a5
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-20 18:15:24 +0530

    MODIFIED: Method name for test case

--- test/QuantityMeasurementTest.js
+++ test/QuantityMeasurementTest.js
@@ -8,7 +8,8 @@ describe("Quantity Measurement", () => {
     it("given0FeetAnd0Inch_WhenObjectSame_ShouldReturnTrue"), () => {
         let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
         let quantity2 = new QuantityMeasurement(unit.Length.INCH, 0);
-        let result = quantity1 instanceof QuantityMeasurement && quantity2 instanceof QuantityMeasurement;
-        assert.isTrue(result);
+        let result1 = quantity1 instanceof QuantityMeasurement;
+        let result2 = quantity2 instanceof QuantityMeasurement;
+        assert.equal(result1, result2);
     } 
 });
