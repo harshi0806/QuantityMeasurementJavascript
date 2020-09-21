81109338c9171f9b85a2f2b301a23ba6137e8c65
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-22 02:00:11 +0530

    ADDED: Code for Yard Comparison

--- main/Unit.js
+++ main/Unit.js
@@ -6,8 +6,9 @@
 class Unit {
     //Base unit for Length conversion is Inch
     Length = {
-        Feet: 12,
-        INCH: 1, 
+        FEET: 12,
+        INCH: 1,
+        YARD: 36, 
     };
 }
 
