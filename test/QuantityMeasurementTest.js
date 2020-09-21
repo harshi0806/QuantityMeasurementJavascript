33bba620821623569104920e09cba8b09a694e72
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-22 00:45:21 +0530

    ADDED: Test case for checking Reference for Feet

--- test/QuantityMeasurementTest.js
+++ test/QuantityMeasurementTest.js
@@ -5,23 +5,33 @@ var quantityMeasurement = new QuantityMeasurement();
 var unit = new Unit();
 
 describe("Quantity Measurement", () => {
-    it("given0FeetAnd0Inch_WhenCheckedForType_ShouldReturnEqual"), () => {
+    it("given0FeetAnd0Inch_WhenCheckedForType_ShouldReturnEqual"), (callback) => {
         let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
         let quantity2 = new QuantityMeasurement(unit.Length.INCH, 0);
         let result1 = quantity1 instanceof QuantityMeasurement;
         let result2 = quantity2 instanceof QuantityMeasurement;
         assert.equal(result1, result2);
+        callback();
     } 
-    it("given0FeetAnd1Feet_WhenCompared_ShouldReturnFalse"), () => {
+    it("given0FeetAnd1Feet_WhenCompared_ShouldReturnFalse"), (callback) => {
         let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
         let quantity2 = new QuantityMeasurement(unit.Length.Feet, 1);
         let compareCheck = quantityMeasurement.compare(quantity1, quantity2);
         assert.isNotTrue(compareCheck);
+        callback();
     }
-    it("given0FeetAnd0Feet_WhenCompared_ShouldReturnTrue"), () => {
+    it("given0FeetAnd0Feet_WhenCompared_ShouldReturnTrue"), (callback) => {
         let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
         let quantity2 = new QuantityMeasurement(unit.Length.Feet, 0);
         let compareCheck = quantityMeasurement.compare(quantity1, quantity2);
         assert.isTrue(compareCheck);
-    }  
+        callback();
+    }
+    it("given0FeetAnd0Feet_WhenComparedForSameReference_ShouldReturnFalse"), (callback) => {
+        let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
+        let quantity2 = new QuantityMeasurement(unit.Length.INCH, 0);
+        let referenceCheck = quantity1 === quantity2;
+        assert.isNotTrue(referenceCheck);
+        callback();
+    }   
 });
