2a4f4728d6fbcde07c53332d78ff6f46eb6c27c6
harshi0806 <harshitashrivatav121@gmail.com>
2020-09-22 01:28:45 +0530

    ADDED: Test case to compare for Equality of two inches

--- test/QuantityMeasurementTest.js
+++ test/QuantityMeasurementTest.js
@@ -6,7 +6,7 @@ var unit = new Unit();
 
 describe("Quantity Measurement", () => {
     it("given0FeetAnd0Inch_WhenCheckedForValue_ShouldReturnEqual"), (callback) => {
-        let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
+        let quantity1 = new QuantityMeasurement(unit.Length.FEET, 0);
         let quantity2 = new QuantityMeasurement(unit.Length.INCH, 0);
         let result1 = quantity1 instanceof QuantityMeasurement;
         let result2 = quantity2 instanceof QuantityMeasurement;
@@ -14,35 +14,35 @@ describe("Quantity Measurement", () => {
         callback();
     } 
     it("given0FeetAnd1Feet_WhenCompared_ShouldReturnFalse"), (callback) => {
-        let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
-        let quantity2 = new QuantityMeasurement(unit.Length.Feet, 1);
+        let quantity1 = new QuantityMeasurement(unit.Length.FEET, 0);
+        let quantity2 = new QuantityMeasurement(unit.Length.FEET, 1);
         let compareCheck = quantityMeasurement.compare(quantity1, quantity2);
         assert.isNotTrue(compareCheck);
         callback();
     }
     it("given0FeetAnd0Feet_WhenCompared_ShouldReturnTrue"), (callback) => {
-        let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
-        let quantity2 = new QuantityMeasurement(unit.Length.Feet, 0);
+        let quantity1 = new QuantityMeasurement(unit.Length.FEET, 0);
+        let quantity2 = new QuantityMeasurement(unit.Length.FEET, 0);
         let compareCheck = quantityMeasurement.compare(quantity1, quantity2);
         assert.isTrue(compareCheck);
         callback();
     }
     it("given0FeetAnd0Feet_WhenCheckedForSameReference_ShouldReturnFalse"), (callback) => {
-        let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
+        let quantity1 = new QuantityMeasurement(unit.Length.FEET, 0);
         let quantity2 = new QuantityMeasurement(unit.Length.INCH, 0);
         let referenceCheck = quantity1 === quantity2;
         assert.isNotTrue(referenceCheck);
         callback();
     } 
     it("givenFeet_WhenCheckedForNull_ShouldReturnEqual"), (callback) => {
-        let quantity1 = new QuantityMeasurement(unit.Length.Feet, null);
+        let quantity1 = new QuantityMeasurement(unit.Length.FEET, null);
         let nullCheck = quantity1.checkNullValue();
         assert.equal(nullCheck, "Value is null or undefined");
         callback();
     } 
     it("givenTwoFeet_WhenCheckedForType_ShouldReturnEqual"), (callback) => {
-        let quantity1 = new QuantityMeasurement(unit.Length.Feet, 0);
-        let quantity2 = new QuantityMeasurement(unit.Length.Feet, 0);
+        let quantity1 = new QuantityMeasurement(unit.Length.FEET, 0);
+        let quantity2 = new QuantityMeasurement(unit.Length.FEET, 0);
         assert.equal(typeof(quantity1), typeof(quantity2));
         callback();
     }
@@ -54,4 +54,18 @@ describe("Quantity Measurement", () => {
         assert.equal(result1, result2);
         callback();
     }  
+    it("given0InchAnd1Inch_WhenCompared_ShouldReturnFalse"), (callback) => {
+        let quantity1 = new QuantityMeasurement(unit.Length.INCH, 0);
+        let quantity2 = new QuantityMeasurement(unit.Length.INCH, 1);
+        let compareCheck = quantityMeasurement.compare(quantity1, quantity2);
+        assert.isNotTrue(compareCheck);
+        callback();
+    }
+    it("given0InchAnd0Inch_WhenCompared_ShouldReturnTrue"), (callback) => {
+        let quantity1 = new QuantityMeasurement(unit.Length.INCH, 0);
+        let quantity2 = new QuantityMeasurement(unit.Length.INCH, 0);
+        let compareCheck = quantityMeasurement.compare(quantity1, quantity2);
+        assert.isTrue(compareCheck);
+        callback();
+    }
 });
